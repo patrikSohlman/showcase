a7c3ba3e5bc1f4f2f44af6c0bfcddf2629b9f19d
patrikSohlman <patrik.sohlman@hotmail.se>
2020-08-24 09:13:12 +0200

    Initial coding commit

---
+++ app/client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+        <home-image></home-image>
+
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+import HomeImage from '@/components/HomeImage';
+export default {
+    name: 'App',
+    components: {
+        AppNavigation,
+        HomeImage
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ app/client/src/components/HomeImage.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container fluid fill-height class="home-image">
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-4 font-weight-black white--text text-xs-center">PATRIK SOHLMAN</div>
+            <div class="display-4 font-weight-black white--text text-xs-center mb-3">Developer of IoT solutions</div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">A site for your curious eyes, and a site for my curious mind.</div>
+            <v-btn fab class="mt-5 white darken-4">
+            <v-icon >mdi-arrow-expand-down</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeImage'
+};
+</script>
+
+<style scoped>
+.home-image {
+    background: url('http://source.unsplash.com/9drS5E_Rguc');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
